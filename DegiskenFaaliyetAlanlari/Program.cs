c6641d3d6e167baa00c5bf2605f84bed7aaebd03
Halil Özcan <halil_ozcann@outlook.com>
2024-12-11 13:32:29 +0300

    C# Degisken Faaliyet Alanları Yapıldı.

--- DegiskenFaaliyetAlanlari/Program.cs
+++ DegiskenFaaliyetAlanlari/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,21 @@ namespace DegiskenFaaliyetAlanlari
 {
     internal class Program
     {
+
+        static string s2 = "Hasan";
         static void Main(string[] args)
         {
+             string s1 = "Halil";
+            s1 = "Yakup";
+
+            s2 = s1;
+
+        }
+
+        static void Test()
+        {
+            s2 = "Yavuz";
+            // s2 değeri gloabal değişken olduğu için her iki metotda kullanabildik. ama s1 sadece bir metota ait bir yerde tanımlı olduğu için test metodunda çağıramadık.
         }
     }
 }
