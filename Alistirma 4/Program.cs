9ec7529550a93b4060a752ee4de4a98c13e62279
Halil Özcan <halil_ozcann@outlook.com>
2024-12-11 17:56:03 +0300

    Alistirma 4 Yapıldı

--- Alistirma 4/Program.cs
+++ Alistirma 4/Program.cs
@@ -10,6 +10,18 @@ namespace Alistirma_4
     {
         static void Main(string[] args)
         {
+            //Kullanıcıdan bir string tarih değeri alarak bunu DateTime tipine dönüştürüp haftanın hangi gününe denk geldiğini gösterin.
+
+            Console.WriteLine("Merhaba");
+
+            Console.Write("Lütfen bir tarih değeri giriniz : ");
+            string date = Console.ReadLine();
+
+            Console.Clear();
+
+            DateTime date1 = Convert.ToDateTime(date);
+
+            Console.WriteLine("sonuc : " + date1.DayOfWeek);
         }
     }
 }
