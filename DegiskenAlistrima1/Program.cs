1752078727a3245fd49bbb0f0e99fc910808d9d2
Halil Özcan <halil_ozcann@outlook.com>
2024-12-11 13:43:28 +0300

    C# Degisken Alistrima 1 Yapıldı

--- DegiskenAlistrima1/Program.cs
+++ DegiskenAlistrima1/Program.cs
@@ -10,7 +10,24 @@ namespace DegiskenAlistrima1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Merhaba");
 
+            Console.Write("İsminiz Nedir? ");
+            string isim = Console.ReadLine();
+
+            Console.Write("Soyisminiz Nedir? ");
+            string soyisim = Console.ReadLine();
+
+            Console.Write("Yasadığınız Şehri Girin ");
+            string sehir = Console.ReadLine();  
+
+            Console.Clear();
+            
+            Console.WriteLine("isim: " + isim);
+            Console.WriteLine("soyism: " + soyisim);
+            Console.WriteLine("sehir: " + sehir);
+
+            Console.ReadLine(); 
         }
     }
 }
