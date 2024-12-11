0abe370066f44a4bc7fc9f18efa19fb77a525002
Halil Özcan <halil_ozcann@outlook.com>
2024-12-11 17:23:08 +0300

    Alistirma 1 Yapıldı

--- Alistirma 1/Program.cs
+++ Alistirma 1/Program.cs
@@ -10,6 +10,19 @@ namespace Alistirma_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Merhaba");
+
+            Console.WriteLine("Bana bir mesaj notu yazar mısınız?");
+
+            string mesaj = Console.ReadLine();
+
+            int mesajdonusum = Convert.ToInt32(mesaj.Length);
+
+            decimal sonuc = mesajdonusum * 10;
+
+            Console.WriteLine("sonuc : " + sonuc);
+            
+            Console.ReadLine();
         }
     }
 }
