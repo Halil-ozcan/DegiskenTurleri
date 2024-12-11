ff1d64f174f4804c2c02de96b989722ead85c37c
Halil Özcan <halil_ozcann@outlook.com>
2024-12-11 14:42:57 +0300

    C# Degsiken Alistirma 2 Yapıldı.

--- DegiskenAlistirmalar2/Program.cs
+++ DegiskenAlistirmalar2/Program.cs
@@ -8,8 +8,38 @@ namespace DegiskenAlistirmalar2
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Merhaba");
+
+            Console.Write("İsiminizi Giriniz: ");
+            string isim = Console.ReadLine();
+
+            Console.Write("Soyisminizi Giriniz: ");
+            string soyisim = Console.ReadLine();
+
+            Console.Write("Şehrinizi Giriniz: ");
+            string sehir = Console.ReadLine();
+
+
+            Console.Write("Yaşınızı Giriniz: ");
+            
+            int yas = Convert.ToInt32(Console.ReadLine());
+
+
+            
+            Console.Clear();
+
+
+            Console.WriteLine("İsim : " + isim);
+            Console.WriteLine("Soyisim : " + soyisim);
+            Console.WriteLine("Şehir : " + sehir);
+            Console.WriteLine("Yaş : " + yas);
+
+            Console.ReadLine();
+
         }
     }
 }
