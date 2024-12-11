977e0e5601b5a9f1a46862a92c9df25307844149
Halil Özcan <halil_ozcann@outlook.com>
2024-12-11 17:13:46 +0300

    Alistirma

--- DegiskenAlistirmalar2/Program.cs
+++ DegiskenAlistirmalar2/Program.cs
@@ -10,6 +10,34 @@ namespace DegiskenAlistirmalar2
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Merhaba");
+
+            Console.Write("İsiminizi Giriniz: ");
+            string isim = Console.ReadLine();
+
+            Console.Write("Soyisminizi Giriniz: ");
+            string soyisim = Console.ReadLine();
+
+            Console.Write("Şehrinizi Giriniz: ");
+            string sehir = Console.ReadLine();
+
+
+            Console.Write("Yaşınızı Giriniz: ");
+            
+            int yas = Convert.ToInt32(Console.ReadLine());
+
+            
+            Console.Clear();
+
+
+            Console.WriteLine("İsim : " + isim);
+            Console.WriteLine("Soyisim : " + soyisim);
+            Console.WriteLine("Şehir : " + sehir);
+            Console.WriteLine("Yaş : " + yas);
+
+            Console.ReadLine();
+
         }
     }
 }
