86debcd86e3e06a7a6ff1c173f9f23f0052fb8ac
Halil Özcan <halil_ozcann@outlook.com>
2024-12-11 15:07:39 +0300

    C# Degisken Alistirma 3 Yapıldı.

--- DegiskenlerAlistirma3/Program.cs
+++ DegiskenlerAlistirma3/Program.cs
@@ -10,6 +10,29 @@ namespace DegiskenlerAlistirma3
     {
         static void Main(string[] args)
         {
+            // string olarak verilen değeri bool tipine dönüştürün;
+
+            string metin1 = "True";
+
+            bool b2 = Convert.ToBoolean(metin1);
+
+            // Soru2: int içerisinde olan 100 değerini byte ve float tiplerine dönüştürün;
+
+            int s1 = 100;
+
+            byte b1 = (byte) s1;
+            float f1 = s1;
+
+            // Soru3: byte değişken içerisinde olan tipi decimal değişken tipine dönüştürün
+
+            byte b3 = 10;
+
+            decimal d1 = b3;
+
+
+
+
+
         }
     }
 }
