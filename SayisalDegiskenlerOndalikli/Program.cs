46fda5cfcccdb4462c050714e1040465438c3940
Halil Özcan <halil_ozcann@outlook.com>
2024-12-11 13:07:59 +0300

    C# Double, Decimal, Float Ondalikli Veri Tipleri Yapıldı.

--- SayisalDegiskenlerOndalikli/Program.cs
+++ SayisalDegiskenlerOndalikli/Program.cs
@@ -10,6 +10,36 @@ namespace SayisalDegiskenlerOndalikli
     {
         static void Main(string[] args)
         {
+            // Double
+
+            double doubleVal1 = 10.2;
+            double doubleVal2 = 10;
+            double doubleVal3 = 20.5d;
+
+            double doubleMin = double.MinValue;
+            double doubleMax = double.MaxValue;
+
+            // Decimal
+
+            decimal decimalVal = 10.2M;
+
+            decimal decimalMin = decimal.MinValue;
+            decimal decimalMax = decimal.MaxValue;
+
+            // Float
+
+            float floatVal = 20.5f;
+            float floatMin = float.MinValue;
+            float floatMax = float.MaxValue;
+
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
+
+            Console.WriteLine(decimal.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+
+            Console.WriteLine(floatMin);
+            Console.WriteLine(floatMax);
         }
     }
 }
